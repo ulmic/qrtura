5ee171eb87f4e16e7f0ff8d913ec559366e85509
pavel <kalashnikovisme@gmail.com>
2014-01-16 06:36:14 +0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qrtura::Application.config.secret_token = '375e09d22b7ec9d9a2866ff17feded2c7ed5d38e6d5b96cf96a613d0e0febba03ab025876f66427d75e81a0f9764e63c04103fce9610665d72045eb124436b8f'
