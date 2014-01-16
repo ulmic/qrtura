7b23478772bd88b5c98fd95a043c5f60a008471b
pavel <kalashnikovisme@gmail.com>
2014-01-16 07:38:28 +0400

    add admin route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Qrtura::Application.routes.draw do
   root to: 'welcome#index'
   resources :pages, only: :show
 
+  get 'admin' => "admin/sessions#new"
   namespace :admin do
     resource :sessions, only: [ :new, :create, :destroy ]
     resources :pages
